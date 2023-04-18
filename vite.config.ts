daec84ef16c15f068cb1513387618e25b05e7bfc
Lochlan <lbernard@nzigen.com>
2023-04-18 09:44:29 +0900

    set base url for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: "/intersection_observer_API_study/",
   plugins: [solidPlugin()],
   server: {
     port: 3000,
